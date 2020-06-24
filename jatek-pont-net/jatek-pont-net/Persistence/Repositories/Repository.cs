3c34f00ec5b9770cb72b19faf1ef46683f52e8bd
Szucs Nikolett <lexi.nikolett.szucs@gmail.com>
2020-06-24 20:31:16 +0200

    implement base repository

---
+++ jatek-pont-net/jatek-pont-net/Persistence/Repositories/Repository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using jatek_pont_net.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace jatek_pont_net.Persistence.Repositories
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        protected readonly jatek_pont_netContext _context;
+
+        public Repository(jatek_pont_netContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<TEntity>> Find(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await _context.Set<TEntity>().Where(predicate).ToListAsync();
+        }
+
+        public async Task<List<TEntity>> GetAll()
+        {
+            return await _context.Set<TEntity>().ToListAsync();
+        }
+
+        public async Task<TEntity> GetById(int id)
+        {
+            return await _context.Set<TEntity>().FindAsync(id);
+        }
+    }
+}
