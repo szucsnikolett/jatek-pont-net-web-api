b1eae879621091cd2230ee0e77dc341b989a0a57
Szucs Nikolett <lexi.nikolett.szucs@gmail.com>
2020-06-26 01:58:02 +0200

    create crud interface

---
+++ jatek-pont-net/jatek-pont-net/Domain/Repositories/ICrud.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace jatek_pont_net.Domain.Repositories
+{
+    internal interface ICrud<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        Task<TEntity> Add(TEntity entity);
+
+        Task<TEntity>Edit(TEntity entity);
+        
+        void Remove(TEntity entity);
+    }
+}
