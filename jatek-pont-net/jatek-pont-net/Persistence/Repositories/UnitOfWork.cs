ae823990897fa417035ca9ab2ab2837e06a74fe2
Szucs Nikolett <lexi.nikolett.szucs@gmail.com>
2020-06-26 02:01:44 +0200

    implement unit of work

---
+++ jatek-pont-net/jatek-pont-net/Persistence/Repositories/UnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using jatek_pont_net.Domain.Repositories;
+
+namespace jatek_pont_net.Persistence.Repositories
+{
+    internal class UnitOfWork : IUnitOfWork
+    {
+        private readonly jatek_pont_netContext _context;
+        public IArRepository ArRepository { get; private set; }
+
+        public UnitOfWork(jatek_pont_netContext context,
+                            IArRepository ar)
+        {
+            _context = context;
+            ArRepository = ar;
+        }
+        public async Task Commit()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
